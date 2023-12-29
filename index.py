c8b91644d1a3c44e7c1d9e1f7621ac5c74c14abf
JhonZ-Dev <jhonmacias1999@gmail.com>
2023-12-28 21:39:59 -0500

    Update index.py

--- index.py
+++ index.py
@@ -5,3 +5,9 @@ class TaskManager:
     def __init__(self, filename="tasks.pkl"):
         self.filename = filename
         self.tasks = self.load_tasks()
+        
+    def load_tasks(self):
+        if os.path.exists(self.filename):
+            with open(self.filename, 'rb') as file:
+                return pickle.load(file)
+        return []
